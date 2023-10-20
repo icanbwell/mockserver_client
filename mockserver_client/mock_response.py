7d5164a0f4a04f6f6fa94997324096bf6b262fea
Imran Qureshi <imranq2@hotmail.com>
2023-10-20 00:02:53 -0700

    add test

--- mockserver_client/mock_response.py
+++ mockserver_client/mock_response.py
@@ -4,7 +4,10 @@ from typing import Dict, Any, List, Optional
 
 class MockResponse:
     def __init__(self, *, response: Dict[str, Any]) -> None:
-        self.raw_body: Optional[str] = response.get("body")
+        raw_body = response.get("body")
+        if isinstance(raw_body, dict):
+            raw_body = json.dumps(raw_body)
+        self.raw_body: Optional[str] = raw_body
         self.json_body: Dict[str, Any] | List[Dict[str, Any]] | None
         if not self.raw_body:
             self.json_body = None
